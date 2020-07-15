6684cd56b5f042edc38dc50eb1cfb7c2e467570f
Shomikghosh <59383763+Shomikghosh@users.noreply.github.com>
2020-07-15 17:08:16 +0530

    refracting

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname+"/date.js");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-const items = ["Buy Food","Cook Food","Eat Food"];
-const workItems = [];
-
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({etended:true}));
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-    let day = date();
-    res.render("list",{listTitle:day,newListItems:items});
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+app.get("/", function(req, res) {
+
+const day = date.getDate();
+
+  res.render("list", {listTitle: day, newListItems: items});
+
 });
 
 app.post("/", function(req, res){
 
-    const item = req.body.newItem;
-    if (req.body.list === "Work") {
-      workItems.push(item);
-      res.redirect("/work");
-    } else {
-      items.push(item);
-      res.redirect("/");
-    }
-  });
+  const item = req.body.newItem;
 
-
-app.get("/work",function(req,res){
-    res.render("list",{listTitle:"Work list",newListItems:workItems});
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
-app.get("/about",function(req,res){
-    res.render("about");
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
-
-app.listen(3000,function(){
-    console.log("Server up and running in port 3000");
+app.get("/about", function(req, res){
+  res.render("about");
 });
 
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
--- date.js
+++ date.js
@@ -1,27 +1,28 @@
-module.exports=getDate;
-function getDate(){
-    var today = new Date();
-    var options ={
-        weekday: "long",
-        day:"numeric",
-        month:"long"
+//jshint esversion:6
+
+exports.getDate = function() {
+
+    const today = new Date();
+  
+    const options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long"
     };
-
-    return day = today.toLocaleDateString("en-US",options); 
-
-}
-
-  exports.getDay=
-   function()
-   {
-    let today = new Date();
-    let options ={
-        weekday: "long"
+  
+    return today.toLocaleDateString("en-US", options);
+  
+  };
+  
+  exports.getDay = function () {
+  
+    const today = new Date();
+  
+    const options = {
+      weekday: "long"
     };
-    return day = today.toLocaleDateString("en-US",options); 
-    const day = today.toLocaleDateString("en-US",options); 
-    
-
-    return day;
-}
-
+  
+    return today.toLocaleDateString("en-US", options);
+  
+  };
+  
